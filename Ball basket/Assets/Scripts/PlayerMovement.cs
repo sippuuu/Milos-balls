dfae09178b402b115f48579a82cb3865d3c24520
Mihai Niculae <mmihai97@gmail.com>
2022-07-31 18:25:34 +0300

    patch 0.2

--- Ball basket/Assets/Scripts/PlayerMovement.cs
+++ Ball basket/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private Rigidbody rb;
+    public float jumpForce = 3.0f;
+    public bool isGrounded;
+    Vector3 jump;
+
+    Vector2 rotation = Vector2.zero;
+    public float lookspeed = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0, 2.0f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Movement();
+        Camera();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    void Camera()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
